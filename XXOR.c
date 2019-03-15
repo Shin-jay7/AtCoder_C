0e734abf4a2656390cded8112e5f11af437b412a
Shin Ikeji <shin@jay7.net>
2019-03-14 17:48:00 -0700

    add new file

---
+++ XXOR.c
@@ -0,0 +1,39 @@
+// Task: https://atcoder.jp/contests/abc117/tasks/abc117_d
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+  int N,i,j,sum[40];
+  for (i=0; i<40; i++) sum[i]=0;
+  long K,A[100000],A_[100000],ans=0;
+  long long func=0;
+
+  scanf("%d %ld", &N,&K);
+  for (i=0; i<N; i++) {
+    scanf("%ld", &A[i]);
+    A_[i] = A[i];
+  }
+
+  // Add up each binary digits???
+  for(i=0; i<N; i++) {
+    for(j=0; j<40; j++) {
+      sum[j] += A_[i]%2;
+      A_[i] = A_[i] >> 1;
+    }
+  }
+
+  for (i=39; i>-1; i--) {
+    // What's going on???????????
+    if (sum[i]*2 < N) {
+      if (pow(2.0,i)+ans <= K) {
+        ans += (long)pow(2.0,i);
+      }
+    }
+  }
+
+  for (i=0; i<N; i++) {
+    func += ans^A[i];
+  }
+  printf("%lld\n", func);
+}
+
